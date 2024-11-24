7ddc0ca61a13044d65405a0c462244ee5bb232a5
Adelyn Arens <adelynarens@Adelyns-MacBook-Air.local>
2024-11-24 16:07:18 -0500

    update karma.config

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,19 +1,41 @@
 module.exports = function (config) {
     config.set({
       basePath: '',
-      frameworks: ['jasmine'],
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
       plugins: [
-        'karma-chrome-launcher',
-        'karma-jasmine',
-        'karma-jasmine-html-reporter',
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
       ],
       client: {
-        clearContext: false,
+        clearContext: false
       },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' },
+          { type: 'json-summary' }
+        ],
+        check: {
+          global: {
+            statements: 90,
+            branches: 90,
+            functions: 90,
+            lines: 90
+          }
+        }
+      },
+      reporters: ['progress', 'kjhtml', 'coverage'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
       browsers: ['ChromeHeadless'],
-      singleRun: true,
-      restartOnFileChange: true,
-      reporters: ['progress', 'kjhtml'],
+      singleRun: false,
+      restartOnFileChange: true
     });
   };
-  
