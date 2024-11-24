bc3e6f63b9ac3a7b4820623db24b57013bfbbc84
adelynarens <118165079+adelynarens@users.noreply.github.com>
2024-11-24 16:23:55 -0500

    RTP-8 Configure Checks For Unit Tests (#6)
    * update test script to include coverage and only run in terminal
    
    * add github workflows for unit tests and code coverage
    
    * update github workflows
    
    * small update to workflows
    
    * update karma.config
    
    * another small update
    
    * change ChromeHeadlessCI to ChromeHeadless
    
    ---------
    
    Co-authored-by: Adelyn Arens <adelynarens@Adelyns-MacBook-Air.local>

---
+++ client/karma.conf.js
@@ -0,0 +1,45 @@
+// karma.conf.js
+module.exports = function (config) {
+    config.set({
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
+      plugins: [
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
+      ],
+      client: {
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' }
+        ]
+      },
+      reporters: ['progress', 'kjhtml', 'coverage'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: false,
+      browsers: ['Chrome', 'ChromeHeadless'],
+      customLaunchers: {
+        ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: [
+            '--no-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--remote-debugging-port=9222'
+          ]
+        }
+      },
+      singleRun: true,
+      restartOnFileChange: false
+    });
+  };
+  
