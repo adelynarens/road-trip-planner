43356dbbb6a23a3983c864c2aa0622b339680b76
Adelyn Arens <adelynarens@Adelyns-MacBook-Air.local>
2024-11-24 16:16:21 -0500

    another small update

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,3 +1,4 @@
+// karma.conf.js
 module.exports = function (config) {
     config.set({
       basePath: '',
@@ -10,32 +11,35 @@ module.exports = function (config) {
         require('@angular-devkit/build-angular/plugins/karma')
       ],
       client: {
-        clearContext: false
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
       },
       coverageReporter: {
         dir: require('path').join(__dirname, './coverage'),
         subdir: '.',
         reporters: [
           { type: 'html' },
-          { type: 'text-summary' },
-          { type: 'json-summary' }
-        ],
-        check: {
-          global: {
-            statements: 90,
-            branches: 90,
-            functions: 90,
-            lines: 90
-          }
-        }
+          { type: 'text-summary' }
+        ]
       },
       reporters: ['progress', 'kjhtml', 'coverage'],
       port: 9876,
       colors: true,
       logLevel: config.LOG_INFO,
-      autoWatch: true,
-      browsers: ['ChromeHeadless'],
-      singleRun: false,
-      restartOnFileChange: true
+      autoWatch: false,
+      browsers: ['Chrome', 'ChromeHeadless'],
+      customLaunchers: {
+        ChromeHeadlessCI: {
+          base: 'ChromeHeadless',
+          flags: [
+            '--no-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--remote-debugging-port=9222'
+          ]
+        }
+      },
+      singleRun: true,
+      restartOnFileChange: false
     });
   };
+  
